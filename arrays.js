2c021995cda76036487e060b9102b7f2124925f9
gaurav <gauravdhall35@gmail.com>
2024-09-04 22:01:33 +0530

    learnt arrays

---
+++ arrays.js
@@ -0,0 +1,16 @@
+let arr = [1,2,3,4,5,6];
+       //  0 1 2 3 4 5
+// console.log(arr,typeof arr);
+// console.log(arr.length);
+
+// arr[0]=45;
+// console.log(arr[0]);
+// console.log(arr[3]);
+// console.log(arr[5]);
+
+console.log(arr.toString());
+console.log(arr.join("-"));
+console.log(arr.pop());
+console.log(arr);
+
+
