8fc42dd4ed512746cf399b23ab790d7bb8cbd60f
gaurav <gauravdhall35@gmail.com>
2024-09-09 15:44:45 +0530

    more practise

--- arrays.js
+++ arrays.js
@@ -1,16 +1,54 @@
-let arr = [1,2,3,4,5,6];
-       //  0 1 2 3 4 5
-// console.log(arr,typeof arr);
-// console.log(arr.length);
+// let arr = [1,2,3,4,5,6];
+//        //  0 1 2 3 4 5
+// // console.log(arr,typeof arr);
+// // console.log(arr.length);
 
-// arr[0]=45;
-// console.log(arr[0]);
-// console.log(arr[3]);
-// console.log(arr[5]);
+// // arr[0]=45;
+// // console.log(arr[0]);
+// // console.log(arr[3]);
+// // console.log(arr[5]);
 
-console.log(arr.toString());
-console.log(arr.join("-"));
-console.log(arr.pop());
-console.log(arr);
+// // console.log(arr.toString());
+// // console.log(arr.join("-"));
+// // console.log(arr.pop());
+// // console.log(arr);
+
+// // let b="gauri"
+
+// // b[0]="p";//strings are immutable
+// // console.log(b)
+
+// // console.log(arr.toString()+typeof(arr))
+// // ;
+// // console.log(typeof(arr))//;arr)
+// // console.log(arr.join(" 😍 "))//replace comma with something
+// // arr.pop();
+// // console.log(arr);
+// arr.push("carryminati");
+// console.log(arr);
+// arr.shift();
+// console.log(arr);
+// // console.log(arr.unshift("ashishchaanchalani"));
+// arr.unshift("ashishchaanchalani");
+// console.log(arr);
+// arr.delete[0];
+// console.log(arr);
+
+// let arr1=[1,2,3,4];
+// let arr2=[5,6,7,8];
+// let arr3=[9,10,11,12];
+
+// console.log(arr1.concat(arr2,arr3));
+// console.log(arr2);
+
+// let arr5=[2,1,5,4];
+// console.log(arr5.sort());
+// console.log(arr5);
+
+arr6=[1,2,3,4,5];
+// console.log()
+arr6.splice(2,3);
+console.log(arr6);
+
 
 
---
+++ pt.js
@@ -0,0 +1,3 @@
+let a=89;//89   a=89
+console.log(a--);  
+// func    (return value)
