f7722696d877311e3555991ca89720101b321c62
gaurav <gauravdhall35@gmail.com>
2025-01-07 09:54:05 +0530

    more on doms

---
+++ targetingelements.js
@@ -0,0 +1,21 @@
+console.log("bhupendra jogi")
+
+// let a=document.getElementsByClassName("box");
+// console.log(a);
+// a[2].style.backgroundColor="red";
+
+// let c=document.getElementById("aquatic");
+// c.style.backgroundColor="aqua";
+
+let d=document.querySelector(".box");
+d.style.backgroundColor="aqua";
+
+console.log(document.querySelectorAll(".box"));
+
+document.querySelectorAll(".box");
+
+document.querySelectorAll(".box").forEach(e => {
+
+   e.style.backgroundColor="red";
+    
+});
