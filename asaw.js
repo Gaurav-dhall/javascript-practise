12e7f7007f5b55898b6317c3cbe3a9d06c5bdf1b
gaurav <gauravdhall35@gmail.com>
2025-01-14 10:35:55 +0530

    more on fetch api and async await

--- asaw.js
+++ asaw.js
@@ -1,10 +1,24 @@
+// async function getdata(){
+//     // simulate getting data from the server
+//     return new Promise((resolve,reject)=>{
+//         setTimeout(() => {
+//             resolve(455);
+//         }, 3500);
+//     })
+// }
 async function getdata(){
-    return new Promise((resolve,reject)=>{
-        setTimeout(() => {
-            resolve(455);
-        }, 3500);
-    })
+   let x= await fetch('https://jsonplaceholder.typicode.com/todos/1')
+    let data = await x.json();
+    // console.log(x,data);
+   return data;
+    // json placeholder is a free fake rest api for testing purpose
 }
+
+// settle means resolve or reject 
+
+// resolve means promise has settled successfully 
+
+// reject means promise has settled with error
 
 async function main(){
       console.log('loading modules');
@@ -12,7 +26,7 @@ async function main(){
     
     console.log('load data');
     
-    let data=await getdata();
+    let data= await getdata();
     console.log(data);
 
     console.log('process data ');
@@ -20,3 +34,9 @@ async function main(){
 }
 
   main();
+
+  // arihant 
+  // educart
+  // bose sir sheet 
+  // ye wala sample paper 
+  // ek paper maine download kiya tha 
