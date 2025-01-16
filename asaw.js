5594188b7e40a938a3cfd6d14f41b816c33fefb2
gaurav <gauravdhall35@gmail.com>
2025-01-16 14:27:17 +0530

    js practise async await

--- asaw.js
+++ asaw.js
@@ -7,7 +7,23 @@
 //     })
 // }
 async function getdata(){
-   let x= await fetch('https://jsonplaceholder.typicode.com/todos/1')
+  //  let x= await fetch('https://jsonplaceholder.typicode.com/todos/1') //get reuqest
+
+  let x= await fetch('https://jsonplaceholder.typicode.com/posts', {
+    method: 'POST',
+    body: JSON.stringify({
+      title: 'foo',
+      body: 'bar',
+      userId: 1,
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  })
+
+  
+ 
+
     let data = await x.json();
     // console.log(x,data);
    return data;
